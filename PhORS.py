64867829da01dbe5fd79f1f017c09b3252d6f34b
Michael Cardiff <mcardiff0321@gmail.com>
2022-04-07 20:59:53 -0500

    Refactor

--- PhORS.py
+++ PhORS.py
@@ -14,28 +14,19 @@ def a_centripetal(t):
 def a_rad(t):
     return max(0,min(a_centripetal(t),g*(1-(t/k_unwind))))
 
-def a_x(t, vx, vy):
-    val = min(0,(a_tan(t)*vx - a_rad(t)*vy)/(np.sqrt(vx*vx+vy*vy)))
-    return val
+def accvec(t, vx, vy):
+    ax = min(0,(a_tan(t)*vx - a_rad(t)*vy)/(np.sqrt(vx*vx+vy*vy)))
+    ay = (a_tan(t)*vy + a_rad(t)*vx)/(np.sqrt(vx*vx+vy*vy))
+    return (ax,ay)
+    
+def posvec(prevx, prevy, vx, vy, dt):
+    return (prevx + (vx*dt),prevy + (vy*dt))
 
-def a_y(t, vx, vy):
-    val = (a_tan(t)*vy + a_rad(t)*vx)/(np.sqrt(vx*vx+vy*vy))
-    return val
-
-def xfun(prevx,vx,dt):
-    return prevx + (vx*dt)
-
-def yfun(prevy,vy,dt):
-    return prevy + (vy*dt)
-
-def vxfun(prevvx,prevax,dt):
-    return prevvx + (prevax*dt)
-
-def vyfun(prevvy,prevay,dt):
-    return prevvy + (prevay*dt)
+def velvec(prevvx, prevvy, prevax, prevay, dt):
+    return (prevvx + (prevax*dt), prevvy + (prevay*dt))
 
 def run():
-    tmax = 5
+    tmax = 3
     dt = 0.05
     vx = [36.36]
     vy = [32.53]
@@ -47,12 +38,9 @@ def run():
     prevx, prevy, prevvx, prevvy, prevax, prevay = x[-1],y[-1],vx[-1],vy[-1],a_x(0.0,vx[-1],vy[-1]),a_y(0.0,vx[-1],vy[-1])
     newx,newy,newvx,newvy,newax,neway=0.0,0.0,0.0,0.0,0.0,0.0
     for t in np.arange(dt, tmax+dt, dt):
-        newvx = vxfun(prevvx,prevax,dt)
-        newvy = vyfun(prevvy,prevay,dt)
-        newax = a_x(t, prevvx, prevvy)
-        neway = a_y(t, prevvx, prevvy)
-        newx = xfun(prevx,newvx,dt)
-        newy = yfun(prevy,newvy,dt)
+        newvx, newvy = velvec(prevvx, prevvy, prevax, prevay, dt)
+        newax, neway = accvec(t, prevvx, prevvy)
+        newx , newy  = posvec(prevx, prevy, newvx, newvy, dt)
         vx.append(newvx)
         vy.append(newvy)
         x.append(newx)
